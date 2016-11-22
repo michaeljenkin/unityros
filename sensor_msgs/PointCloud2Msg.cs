284e53bf12dd4cee7cf750cded4632c9d18239fd
Miquel Massot <miquel.massot@gmail.com>
2016-11-22 18:23:00 +0100

    Fix PointCloud2 decoding

--- sensor_msgs/PointCloud2Msg.cs
+++ sensor_msgs/PointCloud2Msg.cs
@@ -1,11 +1,6 @@
-﻿using System.Collections;
-using System.Text;
-using System.IO;
-using SimpleJSON;
+﻿using SimpleJSON;
 using ROSBridgeLib.std_msgs;
 using UnityEngine;
-
-using PointCloud.PointTypes;
 using PointCloud;
 
 /**
@@ -20,31 +15,36 @@ namespace ROSBridgeLib {
 			private HeaderMsg _header;
 			private uint _height;
 			private uint _width;
-			private PointFieldMsg _fields;
+			private PointFieldMsg[] _fields;
 			private bool _is_bigendian;
 			private bool _is_dense;
 			private uint _point_step;
 			private uint _row_step;
-			private PointCloud<PointXYZRGB> _cloud;
+			private byte[] _data;
+            private PointCloud<PointXYZRGB> _cloud;
 
-
-			public PointCloud2Msg(JSONNode msg) {
+            public PointCloud2Msg(JSONNode msg) {
 				_header = new HeaderMsg (msg ["header"]);
 				_height = uint.Parse(msg ["height"]);
 				_width = uint.Parse(msg ["width"]);
-				_fields = new PointFieldMsg (msg ["fields"]);
 				_is_bigendian = msg["is_bigendian"].AsBool;
 				_is_dense = msg["is_dense"].AsBool;
 				_point_step = uint.Parse(msg ["point_step"]);
 				_row_step = uint.Parse(msg ["row_step"]);
-				_cloud = ReadData(System.Convert.FromBase64String(msg ["data"]));
+                _fields = new PointFieldMsg[msg["fields"].Count];
+                for (int i = 0; i < _fields.Length; i++)
+                {
+                    _fields[i] = new PointFieldMsg(msg["fields"][i]);
+                }
+                _data = System.Convert.FromBase64String(msg["data"]);
+                _cloud = ReadData(_data);
 			}
 
 			public PointCloud2Msg(HeaderMsg header, uint height, uint width, PointFieldMsg fields, bool is_bigendian, uint point_step, uint row_step, byte[] data, bool is_dense) {
 				_header = header;
 				_height = height;
 				_width = width;
-				_fields = fields;
+				//_fields = fields;
 				_is_dense = is_dense;
 				_is_bigendian = is_bigendian;
 				_point_step = point_step;
@@ -53,28 +53,25 @@ namespace ROSBridgeLib {
 			}
 
 			private PointCloud<PointXYZRGB> ReadData(byte[] byteArray) {
-				Stream stream = new MemoryStream(byteArray);
-				BinaryReader b = new BinaryReader(stream);
-
 				PointCloud<PointXYZRGB> cloud = new PointCloud<PointXYZRGB> ();
-
-				switch (_fields.GetDatatype()) {
-				case PointFieldMsg.FLOAT32:
-					for (int i = 0; i < _height * _width; i++) {
-						float x = b.ReadSingle ();
-						float y = b.ReadSingle ();
-						float z = b.ReadSingle ();
-						float rgb = b.ReadSingle ();
-						PointXYZRGB p = new PointXYZRGB (x,y,z,rgb);
-						cloud.Add (p);
-					}
-					break;
-				default:
-					Debug.LogError ("PointCloud datatype not supported!");	
-					break;
+                for (int i = 0; i < _width * _height; i++) {
+                    float x = System.BitConverter.ToSingle(_data, i * (int)_point_step + 0);
+                    float y = System.BitConverter.ToSingle(_data, i * (int)_point_step + 4);
+                    float z = System.BitConverter.ToSingle(_data, i * (int)_point_step + 8);
+                    float rgb = System.BitConverter.ToSingle(_data, i * (int)_point_step + 16);
+                    if (!float.IsNaN(x + y + z))
+                    {
+                        PointXYZRGB p = new PointXYZRGB(x, y, z, rgb);
+                        cloud.Add(p);
+                    }   
 				}
-				return cloud;
+                return cloud;
 			}
+
+            public HeaderMsg GetHeader()
+            {
+                return _header;
+            }
 
 			public uint GetWidth() {
 				return _width;
@@ -104,7 +101,7 @@ namespace ROSBridgeLib {
 				return "PointCloud2 [header=" + _header.ToString() +
 						"height=" + _height +
 						"width=" + _width +
-						"fields=" + _fields.ToString() +
+						//"fields=" + _fields.ToString() +
 						"is_bigendian=" + _is_bigendian +
 						"is_dense=" + _is_dense +
 						"point_step=" + _point_step +
@@ -115,7 +112,7 @@ namespace ROSBridgeLib {
 				return "{\"header\" :" + _header.ToYAMLString() +
 						"\"height\" :" + _height +
 						"\"width\" :" + _width +
-						"\"fields\" :" + _fields.ToYAMLString() +
+						//"\"fields\" :" + _fields.ToYAMLString() +
 						"\"is_bigendian\" :" + _is_bigendian +
 						"\"is_dense\" :" + _is_dense +
 						"\"point_step\" :" + _point_step +
